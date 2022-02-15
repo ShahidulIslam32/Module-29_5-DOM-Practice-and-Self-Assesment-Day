0f10d526d6b09b80c0b06e7ac3deb6f05200a096
shahidul islam <islamkhans148@gmail.com>
2022-02-15 17:00:12 +0600

    UI Done

---
+++ Apply DOM Challange in Leaderborad Site/app.js
@@ -0,0 +1,6 @@
+document.getElementById('top-player').style.color = 'purple';
+
+document.getElementById('players-name').style.backgroundColor = 'rgba(129, 192, 78, 0.77)'
+document.getElementById('players-name').style.padding = '50px'
+document.getElementById('players-name').style.color = '#fff'
+
