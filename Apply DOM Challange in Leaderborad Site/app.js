3377d5a1e30a9f8ac42391ad30c5d5059e002e9a
shahidul islam <islamkhans148@gmail.com>
2022-02-15 23:49:43 +0600

    new item task done

--- Apply DOM Challange in Leaderborad Site/app.js
+++ Apply DOM Challange in Leaderborad Site/app.js
@@ -4,3 +4,14 @@ document.getElementById('players-name').style.backgroundColor = 'rgba(129, 192, 
 document.getElementById('players-name').style.padding = '50px'
 document.getElementById('players-name').style.color = '#fff'
 
+
+let addNew = document.getElementById('add-new').addEventListener('click', function(){
+    let li = document.createElement('li')
+    li.innerText = 'Freelancing'
+    let addDone = document.getElementById('list-container')
+    addDone.appendChild(li)
+})
+
+// input value 
+
+
